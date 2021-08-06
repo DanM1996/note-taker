9976de638d69312cb6c303e88eab4b17ecf51ea4
Dan <bmdmartinez@gmail.com>
2021-08-06 15:58:18 -0400

    fixed some routing issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,12 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
+// takes the notes object stored in db.json and pulls it into here as an object
+const { notes } = require('../Develop/db/db.json');
 
-// router.get('/api/notes', (req, res) => {
-//     res.json(notes);
-// })
+router.get('/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query);
+    res.json(results);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
-// takes the notes object stored in db.json and pulls it into here as an object
-const { notes } = require('./Develop/db/db.json');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -16,14 +13,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // use the routes located in the routes folder
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query);
-    res.json(results);
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
