efa2f45148bff05f80d091875b5c1335a4e3eb2c
Dan <bmdmartinez@gmail.com>
2021-08-07 16:39:07 -0400

    added notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+
+// importing the function written in store.db
 const store = require('../Develop/db/store');
+
 // takes the notes object stored in db.json and pulls it into here as an object
 const { notes } = require('../Develop/db/db.json');
+
 // const params = [req.body.params]
 
 router.get('/notes', (req, res) => {
